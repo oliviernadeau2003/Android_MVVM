74c6332093fc37bcbd982550bd9c2961acc60a9b
Olivier Nadeau <oliviernadeau2003@gmail.com>
2023-02-02 11:33:13 -0500

    Vue du pilot et theme

---
+++ app/src/main/java/ca/qc/cstj/mvvm/core/Constants.kt
@@ -0,0 +1,5 @@
+package ca.qc.cstj.mvvm.core
+
+object Constants {
+    const val EXPERIENCE_PER_LEVEL = 10
+}
---
+++ app/src/main/java/ca/qc/cstj/mvvm/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package ca.qc.cstj.mvvm.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.qc.cstj.mvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
